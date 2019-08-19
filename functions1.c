6b2e8e7b4666c2e497b009fb5e2f9041148370e7
Dioni1195 <dionisio.arango95@gmail.com>
2019-08-19 15:28:16 +0000

    Add Feature : Adding function to concatenate two strings

--- functions1.c
+++ functions1.c
@@ -67,3 +67,32 @@ char *_strcpy(char *dest, char *src)
 	}
 	return (dest);
 }
+
+/**
+  * *_strcat - writes the character c to stdout
+  * @dest: The destination array
+  * @src: The source array
+  *
+  * Return: On success 1.
+  * On error, -1 is returned, and errno is set appropriately.
+  */
+char *_strcat(char *dest, char *src)
+{
+	int tamS, tamD, i, tot, j;
+
+	tamS = 0;
+	j = 0;
+	tamD = 0;
+	while (dest[tamD] != '\0')
+		++tamD;
+	while (src[tamS] != '\0')
+		++tamS;
+	tot = tamD + tamS;
+	for (i = tamD; i <= tot; i++)
+	{
+		dest[i] = src[j];
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
