b36a6378c5b692dddc86ce64e653246fbdaf7578
luiseduardiazc <luis.diaz.car@gmail.com>
2019-08-20 04:00:04 +0000

    fixed infinite loop add free, review betty

--- hsh.c
+++ hsh.c
@@ -9,24 +9,24 @@
 #define BUFERSIZE 50
 
 /**
-  * to_cd - invoke cd command
-  * @path: The path
-  *
-  * Return: A integer.
-  * On error, retunr NULL.
-  */
+ * to_cd - invoke cd command
+ * @path: The path
+ *
+ * Return: A integer.
+ * On error, retunr NULL.
+ */
 int to_cd(char *path)
 {
 	return (chdir(path));
 }
 
 /**
-  * tok_path - Concatenate the input with the path
-  * @token: The token passed
-  *
-  * Return: A pointer.
-  * On error, retunr NULL.
-  */
+ * tok_path - Concatenate the input with the path
+ * @token: The token passed
+ *
+ * Return: A pointer.
+ * On error, retunr NULL.
+ */
 char *tok_path(char *token)
 {
 	char *string, *realPoint;
@@ -44,17 +44,18 @@ char *tok_path(char *token)
 		_strcat(string, token);
 		return (string);
 	}
+	free(string);
 	return (realPoint);
 }
 
 /**
-  * split_input - Split the input
-  * @command: The array that has the commands
-  * @input: The input obtained from getline
-  *
-  * Return: nothing
-  * On error, retunr NULL.
-  */
+ * split_input - Split the input
+ * @command: The array that has the commands
+ * @input: The input obtained from getline
+ *
+ * Return: nothing
+ * On error, retunr NULL.
+ */
 void split_input(char *command[], char *input)
 {
 	char *token;
@@ -77,24 +78,23 @@ void split_input(char *command[], char *input)
  *
  * Return: An integer
  * On error, -1 is returned, and errno is set appropriately.
-*/
+ */
 int loop(char *argv[], char *envp[])
 {
 	pid_t child;
 	char *command[BUFERSIZE], *token, *input = NULL;
-	size_t size;
-	int status, line;
+	size_t line, size = BUFERSIZE;
+	int status;
 
 	while (1)
 	{
 		printf(":) ");
 		line = getline(&input, &size, stdin);
-		/* printf("input %s\n", input); */
 
 		if (line == -1 && line == EOF)
 			break;
 		if (strcmp(input, "exit\n") == 0)
-			exit(EXIT_SUCCESS);
+			free(input), exit(EXIT_SUCCESS);
 
 		split_input(command, input);
 
@@ -112,11 +112,11 @@ int loop(char *argv[], char *envp[])
 			if (execve(command[0], command, NULL))
 			{
 				perror(argv[0]);
-				exit(EXIT_FAILURE);
+				free(input);
+				_exit(EXIT_FAILURE);
 			}
 		}
-		if (child > 0)
-			waitpid(child, &status, WUNTRACED);
+		wait(&status);
 	}
 	free(input);
 	return (status);
@@ -130,7 +130,7 @@ int loop(char *argv[], char *envp[])
  *
  * Return: OStatus
  * On error, -1 is returned, and errno is set appropriately.
-*/
+ */
 int main(int argc, char *argv[], char *envp[])
 {
 	int status;
